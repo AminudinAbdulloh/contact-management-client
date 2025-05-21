b47a76484adfc1cf86ae0bfaa8dbc146a5366ba4
khannedy <echo.khannedy@gmail.com>
2025-05-21 22:33:57 +0700

    Add ContactDetail component to display contact details
    Introduced a new ContactDetail component for viewing detailed information about a contact, including addresses and personal info. Updated the main routing configuration to include the new detail view under the contacts path.

--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,7 @@ import UserLogout from "./components/User/UserLogout.jsx";
 import ContactCreate from "./components/Contact/ContactCreate.jsx";
 import ContactList from "./components/Contact/ContactList.jsx";
 import ContactEdit from "./components/Contact/ContactEdit.jsx";
+import ContactDetail from "./components/Contact/ContactDetail.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -29,6 +30,7 @@ createRoot(document.getElementById('root')).render(
           <Route path="contacts">
             <Route index element={<ContactList/>}/>
             <Route path="create" element={<ContactCreate/>}/>
+            <Route path=":id" element={<ContactDetail/>}/>
             <Route path=":id/edit" element={<ContactEdit/>}/>
           </Route>
 
