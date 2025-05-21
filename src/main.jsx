38e4466de9d103abb091a44c0ab28d0a2ca33a18
khannedy <echo.khannedy@gmail.com>
2025-05-21 20:19:01 +0700

    Add DashboardLayout component with improved navigation
    Introduced a `DashboardLayout` component to provide a structured layout for the dashboard with navigation links for contacts, profile, and logout. Updated routing to use this layout, with a placeholder route for contacts.

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import {BrowserRouter, Route, Routes} from "react-router";
 import Layout from "./components/Layout.jsx";
 import UserRegister from "./components/User/UserRegister.jsx";
 import UserLogin from "./components/User/UserLogin.jsx";
+import DashboardLayout from "./components/DashboardLayout.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -13,8 +14,8 @@ createRoot(document.getElementById('root')).render(
           <Route path="/register" element={<UserRegister/>}/>
           <Route path="/login" element={<UserLogin/>}/>
         </Route>
-        <Route path="/dashboard">
-
+        <Route path="/dashboard" element={<DashboardLayout/>}>
+          <Route path="contacts" element={<div>Contacts</div>}/>
         </Route>
       </Routes>
     </BrowserRouter>
